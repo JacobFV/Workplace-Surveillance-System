d85d260b0ab454c07251295b2d5ab38a591c3861
Jacob Valdez <jacobfv@msn.com>
2019-07-23 12:01:04 -0500

    Add files via upload
    Working prototype for workplace proposal and integration testing

--- main.js
+++ main.js
@@ -1,19 +1,69 @@
-var video = document.querySelector("#videoElement");
+let net;
+const report = document.getElementById('report');
+const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 
-var canvas = document.createElement("canvas");
-canvas.width = video.videoWidth;
-canvas.height = video.videoHeight;
-var ctx = canvas.getContext("2d");
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
 
-//the following block was taken and modified from https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function (stream) {
-        video.srcObject = stream;//.getVideoTracks()[0];
-        video.play();
-      })
-      .catch(function (error) {
-        console.log("Something went wrong!");
-      });
-  }
-//end of quote
+async function app() {
+    console.log('Loading mobilenet..');
+  
+    // Load the model.
+    net = await mobilenet.load();
+    console.log('Sucessfully loaded model');
+  
+    await setupWebcam();
+  
+    // Reads an image from the webcam and associates it with a specific class
+    // index.
+    const addExample = classId => {
+      // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+      // to the KNN classifier.
+      const activation = net.infer(webcamElement, 'conv_preds');
+  
+      // Pass the intermediate activation to the classifier.
+      classifier.addExample(activation, classId);
+    };
+  
+    // When clicking a button, add an example for that class.
+    document.getElementById('ignore').addEventListener('click', () => addExample(0));
+    document.getElementById('trigger').addEventListener('click', () => addExample(1));
+  
+    while (true) {
+      if (classifier.getNumClasses() > 0) {
+        // Get the activation from mobilenet from the webcam.
+        const activation = net.infer(webcamElement, 'conv_preds');
+        // Get the most likely class and confidences from the classifier module.
+        const result = await classifier.predictClass(activation);
+  
+        const classes = ['A', 'B'];
+        report.innerText = result.confidences[1] > 0.5 ? "Detected" : "Ignored";
+        //`
+        //  prediction: ${classes[result.classIndex]}\n
+        //  probability: ${result.confidences[result.classIndex]}
+        //`;
+      }
+  
+      await tf.nextFrame();
+    }
+}
+
+
+app();
