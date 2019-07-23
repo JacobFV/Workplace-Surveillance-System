7c7c2c56a0ab5bbca57086ac359c551be71f70e6
Jacob Valdez <jacobfv@msn.com>
2019-07-22 19:58:46 -0500

    Update main.js
    trying to get iOS to work

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(function (stream) {
         video.srcObject = stream;
+        video.play();
       })
       .catch(function (error) {
         console.log("Something went wrong!");
